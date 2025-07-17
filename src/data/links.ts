b6b4e3ec98c6a4047560d372967aea2bdd7c7310
vishwa.vj <iamvishwajith.s@gmail.com>
2025-07-18 00:49:47 +0530

    resume update

--- src/data/links.ts
+++ src/data/links.ts
@@ -3,7 +3,7 @@ export const Links = {
   linkedin: "https://www.linkedin.com/in/iamvishwajith/",
   email: "mailto:iamvishwajith.s@gmail.com",
   resume:
-    "https://drive.google.com/file/d/1cfuYyqR5iSX622A54hBUJyTdQzNjDayG/view?usp=sharing",
+    "https://drive.google.com/file/d/1-RoasUHASYP38cYCfgN0rsK5FEGXymv-/view?usp=sharing",
   emailonly: "iamvishwajith.s@gmail.com",
   location: "Chennai, India",
   phone: "+91 8122702599",
