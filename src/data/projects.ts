1e6198f55299c5da786c5f4ed2b8573fff5c5aac
vishwa.vj <iamvishwajith.s@gmail.com>
2025-07-26 03:55:29 +0530

    projects update

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -55,26 +55,7 @@ export const projects: Project[] = [
       live: "", // add live URL if hosted
     },
   },
-  {
-    id: 3,
-    title: "Threadly – Reddit-Inspired Community Platform",
-    description:
-      "A full-stack social discussion platform enabling users to create posts with rich content, comment with nested replies, interact within communities and advanced caching for a seamless user experience.",
-    image: "/imgs/threadly.png",
-    tags: [
-      "Next.js",
-      "React",
-      "TypeScript",
-      "Tailwind CSS",
-      "Prisma",
-      "Upstash Redis",
-      "NextAuth",
-    ],
-    links: {
-      github: "https://github.com/vishwa-vj21/Threadly",
-      live: "", //add live URL if hosted
-    },
-  },
+
   {
     id: 4,
     title: "ResumeRadar – AI-Powered Resume Analyzer",
